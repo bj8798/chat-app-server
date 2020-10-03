aa18b726e88faae3dc599609d9980f77853c7e14
Bhargav Joshi (C) <bjoshi@splunk.com>
2020-10-03 19:19:55 +0530

    basic chat functionalities working fine.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const app = require('express')();
+const session = require('express-session');
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 const cors = require('cors');
@@ -16,16 +17,15 @@ const mongoClient = new MongoClient(uri, { useUnifiedTopology: true });
 const dbName = 'chat-app';
 
 mongoClient.connect().then((client) => {
-  console.log('Database connection successfull');
+  console.log('Database connection successful');
   const db = client.db(dbName);
   const users = db.collection('user');
 
-  // Applying needed middlewares
+  // Validating the CORS requests
   app.use(
     cors({
       origin: function (origin, callback) {
-        // allow requests with no origin
-        // (like mobile apps or curl requests)
+        // allow requests with no origin for postman reqests
         console.debug('Request came through:', origin);
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
@@ -36,13 +36,43 @@ mongoClient.connect().then((client) => {
         }
         return callback(null, true);
       },
+      credentials: true,
     })
   );
 
+  // Middle ware for parsing the request object
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  // Middle ware for using the sessions
+  app.use(
+    session({ secret: 'ssshhhhh', saveUninitialized: true, resave: true })
+  );
+
+  app.use('', (req, res, next) => {
+    console.log('At auth middleware.');
+
+    const authPaths = ['/', '/login', '/signup', '/test_session'];
+    if (authPaths.includes(req.path)) {
+      return next();
+    }
+
+    if (!req.session.username) {
+      return res.status(401).send({ message: 'Unauthorized access' });
+    }
+
+    return next();
+  });
+
+  // Below is the previous code which need to be refactored
+  app.get('/', (req, res) => {
+    console.log('get request');
+    req.session.name = 'testname';
+    res.send('Done');
+  });
+
   app.post('/login', (req, res) => {
+    const sess = req.session;
     const username = req.body.username;
     const password = req.body.password;
 
@@ -51,9 +81,14 @@ mongoClient.connect().then((client) => {
       .findOne({ username, password })
       .then((user) => {
         if (user) {
-          res.send({ status: 200 });
+          sess.username = username;
+          sess.fullname = user.fullname;
+          res.status(200).send({
+            message: 'Logged in successfully',
+            fullname: user.fullname,
+          });
         } else {
-          res.send({ status: 401 });
+          res.status(401).send({ message: 'Incorrect Credentials Provided' });
         }
       })
       .catch((err) => {
@@ -61,41 +96,78 @@ mongoClient.connect().then((client) => {
           `Error while finding the User:${username} from the database.`,
           err
         );
-        res.send({ status: 500 });
+        res.status(500).send({ message: 'Internal server error occurred' });
       });
+  });
+
+  app.get('/test_session', (req, res) => {
+    const sess = req.session;
+    if (sess.username) {
+      res
+        .status(200)
+        .send({ username: sess.username, fullname: sess.fullname });
+    } else {
+      res.status(401).send({ message: 'Unauthorized access' });
+    }
+  });
+
+  app.get('/get_active_users', (req, res) => {
+    const activeUsers = {};
+    Object.keys(userSocketMap).forEach((username) => {
+      activeUsers[username] = userSocketMap[username].fullname;
+    });
+    console.log('activeUsers:', activeUsers);
+    res.status(200).send({ activeUsers: activeUsers });
   });
 
   app.post('/signup', (req, res) => {
+    const sess = req.session;
     const username = req.body.username;
     const password = req.body.password;
-    const fullName = req.body.fullName;
+    const fullname = req.body.fullname;
 
-    const userObj = { username, password, fullName };
-    console.log('userObj:', userObj);
+    sess.username = username;
+    sess.fullname = fullname;
+
+    const userObj = { username, password, fullname };
     users
       .insertOne(userObj)
-      .then((signupResp) => {
-        console.log('user added successfully');
-        console.log('response of signup:', signupResp);
-        res.send({ status: 200 });
+      .then(() => {
+        res.status(200).send({ message: 'Signed Up successfully' });
       })
       .catch((err) => {
         console.log('Error while saving the User:', err);
-        res.send({ status: 500 });
+        res.status(500).send({ message: 'Internal server error occurred' });
       });
   });
 
-  app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-  const idObjectMap = {};
+  const userSocketMap = {};
 
   const sendMessage = (fromUser, toUser, message) => {
-    const toSocket = idObjectMap[toUser];
+    const toSocket = userSocketMap[toUser];
     toSocket.emit('received_message', { message: message });
     res.send({ status: 200 });
   };
+
+  io.on('connection', (socket) => {
+    const query = socket.handshake.query;
+    userSocketMap[query.username] = { socket, fullname: query.fullname };
+    console.log('userSocketMap: ', userSocketMap);
+    socket.on('message_sent', (message) => {
+      console.log('message: ', message);
+      console.log('toUser Info:', userSocketMap[message.toUser]);
+
+      if (!userSocketMap[message.toUser]) {
+        return;
+      }
+
+      const toUserSocket = userSocketMap[message.toUser].socket;
+      toUserSocket.emit('message_received', {
+        text: message.text,
+        fromUser: message.fromUser,
+      });
+    });
+  });
 
   app.post('/send_message', (req, res) => {
     const fromUser = req.body.from;
@@ -105,15 +177,6 @@ mongoClient.connect().then((client) => {
     sendMessage(fromUser, toUser, message);
   });
 });
-
-// io.on('connection', (socket) => {
-//   console.log('socket:', socket.id);
-//   idObjectMap[socket.id] = socket;
-//   socket.on('chat message', (msg) => {
-//     console.log('message: ', msg, 'socket id:', socket.id);
-//     socket.broadcast.emit('received', { message: msg });
-//   });
-// });
 
 http.listen(4000, () => {
   console.log('listening on *:4000');
